ff4f2ef96fd7e4a81049eb24db5e80903f666eb9
Lucca <luke.crv@gmail.com>
2021-06-14 01:21:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv').config()
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+})
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
+const getTasks = async () => {
+  const payload = {
+    path: `databases/${databaseId}/query`,
+    method: 'POST',
+  }
+
+  const { results } = await notion.request(payload)
+
+  const tasks = results.map(page => {
+    console.log(page.properties.Description.rich_text[0].text.content);
+
+    console.log(page.properties.Tags);
+
+    return {
+      id: page.id,
+      title: page.properties.Name.title[0].text.content,
+      date: page.properties.Date.date.start,
+      tags: page.properties.Tags.multi_select[0].name,
+      description: page.properties.Description.rich_text[0].text.content,
+    }
+  })
+
+  return tasks;
+}
+
+(async () => {
+  const notionTasks = await getTasks();
+  console.log(notionTasks)
+})();
